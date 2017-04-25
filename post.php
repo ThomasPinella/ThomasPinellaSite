84d8102719da96a5d757fcc9e203f5b411c93056
Thomas Pinella <txpinella@gmail.com>
2017-04-25 15:01:10 +0200

    fixed visitors db issue

--- post.php
+++ post.php
@@ -13,13 +13,13 @@
 
     switch ($convo_time) {
         case 0:
-	    	$sitedata->do_query(insert_new_user($session_id));
+	    	$visitors_db->insert_new_user($session_id);
 	        echo "Hello and welcome to my website, <a href='http://thomaspinella.com'>thomaspinella.com</a>! As you might have guessed, my name is Thomas Pinella. I'm a Computer Science major and rising senior at the University of Rochester.
 
 	What's your name?";
 	        break;
     	case 1:
-    		$sitedata->do_query(add_name($session_id, $clientmsg));
+    		$visitors_db->add_name($session_id, $clientmsg);
 	        echo "Hi ".$clientmsg."! Happy to meet you! It gets dreadfully boring living inside this site sometimes...
 
 	Anyways, the purpose of this conversation is for you to learn a bit about me and vice versa. You're turn, ask me a question!";
