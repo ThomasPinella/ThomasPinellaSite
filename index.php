0c3ed8dee725327020db6105316da346ab9eaf77
Thomas Pinella <txpinella@gmail.com>
2017-04-25 15:20:27 +0200

    log_errors set to 1 now

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
     session_start();
     require_once('Database.php');
     ini_set('display_errors', 1);
+    ini_set('log_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
 ?>
