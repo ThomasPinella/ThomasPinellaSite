39aac559e08b03c393293210a72264e27fe3cb2e
Thomas Pinella <txpinella@gmail.com>
2017-04-25 15:18:39 +0200

    new attempt on error logging

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
     session_start();
     require_once('Database.php');
-    ini_set('error_reporting', E_ALL);
+    ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
 ?>
 
 <html>
--- post.php
+++ post.php
@@ -16,13 +16,13 @@
 	    	$visitors_db->insert_new_user($session_id);
 	        echo "Hello and welcome to my website, <a href='http://thomaspinella.com'>thomaspinella.com</a>! As you might have guessed, my name is Thomas Pinella. I'm a Computer Science major and rising senior at the University of Rochester.
 
-	What's your name?";
+What's your name?";
 	        break;
     	case 1:
     		$visitors_db->add_name($session_id, $clientmsg);
 	        echo "Hi ".$clientmsg."! Happy to meet you! It gets dreadfully boring living inside this site sometimes...
 
-	Anyways, the purpose of this conversation is for you to learn a bit about me and vice versa. You're turn, ask me a question!";
+Anyways, the purpose of this conversation is for you to learn a bit about me and vice versa. You're turn, ask me a question!";
 			break;
     }
     
