2a77c13d7ed21cbde79ce27f702cd08fe5c9b3fb
Thomas Pinella <txpinella@gmail.com>
2017-04-25 16:47:41 +0200

    should work now

--- Visitors_db.php
+++ Visitors_db.php
@@ -3,10 +3,6 @@ class Visitors_db {
 	var $database;
 	function __construct($db) {
 		$this->database = $db;
-        $result = $this->database->do_query("insert into visitors values (DEFAULT, sdkjlf, CURRENT_TIMESTAMP, null, null, null, null);");
-        //while ($row = mysqli_fetch_array($result)) {
-         //   echo $row['name'];
-        //}
 	}
 
 	function get_item($sql, $item_name) {
@@ -19,27 +15,27 @@ class Visitors_db {
 	}
 
 	function insert_new_user($session_id) {
-		$sql = "INSERT INTO visitors VALUES (DEFAULT, ".$session_id.", CURRENT_TIMESTAMP, null, null, null, null);";
+		$sql = "INSERT INTO visitors VALUES (DEFAULT, '".$session_id."', CURRENT_TIMESTAMP, null, null, null, null);";
         $result = $this->database->do_query($sql);
 	}
 
 	function add_name($session_id) {
-		$sql = "UPDATE visitors SET name = '".$name."' WHERE session_id = ".$session_id.";";
+		$sql = "UPDATE visitors SET name = '".$name."' WHERE session_id = '".$session_id."';";
         $result = $this->database->do_query($sql);
 	}
 
 	function add_email($session_id, $email) {
-		$sql = "UPDATE visitors SET email = '".$email."' WHERE session_id = ".$session_id.";";
+		$sql = "UPDATE visitors SET email = '".$email."' WHERE session_id = '".$session_id."';";
         $result = $this->database->do_query($sql);
 	}
 
 	function get_name($session_id) {
-		$sql = "SELECT name FROM visitors WHERE session_id = ".$session_id.";";
+		$sql = "SELECT name FROM visitors WHERE session_id = '".$session_id."';";
 		return $this->get_item($sql, 'name');
 	}
 
 	function get_inspiration($session_id) {
-		$sql = "SELECT inspiration FROM visitors WHERE session_id = ".$session_id.";";
+		$sql = "SELECT inspiration FROM visitors WHERE session_id = '".$session_id."';";
 		return $this->get_item($sql, 'inspiration');
 	}
 }
