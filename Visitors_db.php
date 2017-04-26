6b5b5fa906381c7c57b8559c46414d39e3713eb0
Thomas Pinella <txpinella@gmail.com>
2017-04-26 10:04:54 +0200

    removing single quote from sql statements

--- Visitors_db.php
+++ Visitors_db.php
@@ -20,31 +20,37 @@ class Visitors_db {
 	}
 
 	function add_name($string_id, $name) {
+		$name = str_replace("'", "", $name);
 		$sql = "UPDATE visitors SET name = '".$name."' WHERE string_id = '".$string_id."';";
         $result = $this->database->do_query($sql);
 	}
 
 	function add_question($string_id, $question) {
+		$question = str_replace("'", "", $question);
 		$sql = "UPDATE visitors SET question = '".$question."' WHERE string_id = '".$string_id."';";
         $result = $this->database->do_query($sql);
 	}
 
 	function add_whyhere($string_id, $whyhere) {
+		$whyhere = str_replace("'", "", $whyhere);
 		$sql = "UPDATE visitors SET whyhere = '".$whyhere."' WHERE string_id = '".$string_id."';";
         $result = $this->database->do_query($sql);
 	}
 
 	function add_inspiration($string_id, $inspiration) {
+		$inspiration = str_replace("'", "", $inspiration);
 		$sql = "UPDATE visitors SET inspiration = '".$inspiration."' WHERE string_id = '".$string_id."';";
         $result = $this->database->do_query($sql);
 	}
 
 	function add_qcf($string_id, $qcf) {
+		$qcf = str_replace("'", "", $qcf);
 		$sql = "UPDATE visitors SET qcf = '".$qcf."' WHERE string_id = '".$string_id."';";
         $result = $this->database->do_query($sql);
 	}
 
 	function add_email($string_id, $email) {
+		$email = str_replace("'", "", $email);
 		$sql = "UPDATE visitors SET email = '".$email."' WHERE string_id = '".$string_id."';";
         $result = $this->database->do_query($sql);
 	}
