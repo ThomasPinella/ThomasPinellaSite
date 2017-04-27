ccc555b3b79a3cda35514ae9478123f850f6f0a6
Thomas Pinella <txpinella@gmail.com>
2017-04-27 21:39:53 +0200

    added google analytics script

--- index.php
+++ index.php
@@ -35,6 +35,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Thomas Pinella</title>
         <link type="text/css" rel="stylesheet" href="style.css" />
+        <script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-98196751-1', 'auto');
+			ga('send', 'pageview');
+
+		</script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
         <script>
             $(document).ready(function() {
